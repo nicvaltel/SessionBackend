b6be31f36ae237855e097f689d6d11582cfdc1f9
nicvaltel <nicvaltel@gmail.com>
2025-03-21 20:50:31 +0300

    HTML pages refactoring

---
+++ static/auth.js
@@ -0,0 +1,41 @@
+function login() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  fetch("http://localhost:3000/api/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, password })
+  })
+  .then(response => response.json())
+  .then(data => {
+      document.getElementById("loginMessage").textContent = data.message || data.error;
+  })
+  .catch(error => console.error("Error:", error));
+}
+
+
+function register() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  fetch("http://localhost:3000/api/register", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password })
+  })
+  .then(response => response.json())
+  .then(data => {
+      document.getElementById("registerMessage").textContent = data.message || data.error;
+  })
+  .catch(error => console.error("Error:", error));
+}
+
+
+function verify() {
+  const v_code = document.getElementById("v_code").value;
+  fetch(`http://localhost:3000/api/verify/${v_code}`)
+  .then(response => response.json())
+  .then(data => {
+      document.getElementById("verifyMessage").textContent = data.message || data.error;
+  })
+  .catch(error => console.error("Error:", error));
+}
